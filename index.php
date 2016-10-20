c5e882491798d6ce78f687c5ebdf44a75e88fef3
OTI <otlowski.maciej@gmail.com>
2016-10-21 00:26:59 +0200

    [Init] Make layout of website

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="assets/css/style.css">
+        <!--<link rel="stylesheet" type="text/css" href="css/main.css">-->
+    </head>
+    <body>  
+        <!--HEADER-->
+        <div id="header">  
+            <div class="header-logo"><a href="/"class="header-logo__link">for<span style="color:#FFA17F;">FIT</span>er</a></div>
+            <div class="header-buttons">
+                <button>Log In</button>
+                <button>Sign Up</button>
+            </div>
+        </div>
+        <!--MAIN-->
+        <div id="container">
+            <div class="bg--gradient">
+                <div class="banner"></div>
+            </div>
+            <div class="titles">
+            </div>
+            <div class="title">
+                <div class="title-content_1"></div>
+            </div>
+            <div class="title">
+                <div class="title-content_2"></div>
+            </div>
+            <div class="title">
+                <div class="title-content_3"></div>
+            </div>
+            <div class="title">
+                <div class="title-content_4"></div>
+            </div>
+            CONTAINERRRRRR
+        </div>
+        <!--FOOTER-->
+        <div id="footer">
+            FOOTER
+        </div>
+    </body>
+</html>
+
